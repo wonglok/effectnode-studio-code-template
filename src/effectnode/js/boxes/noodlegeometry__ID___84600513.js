f26bb83376c12d93e7a32b5d59f8ff09fab6506b
wonglok <yellowhappy831@gmail.com>
2021-02-11 08:54:04 +0800

    save

--- src/effectnode/js/boxes/noodlegeometry__ID___84600513.js
+++ src/effectnode/js/boxes/noodlegeometry__ID___84600513.js
@@ -1,8 +1,12 @@
 import { IcosahedronBufferGeometry } from "three";
-export const box = ({ pulse, onUserData }) => {
-  pulse({
-    geometry: new IcosahedronBufferGeometry(10, 2),
-  });
+export const box = ({ ...relay }) => {
+  let setup = () => {
+    relay.pulse({
+      geometry: new IcosahedronBufferGeometry(10, 2),
+    });
+  };
+
+  setup();
 
   return {
     name: "geo",
